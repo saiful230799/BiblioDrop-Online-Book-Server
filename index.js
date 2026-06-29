5a5d3d4bc5b941aa3171fa53d8b1993a8753ff5a
saiful230799 <mirajvai24@gmail.com>
2026-06-29 22:12:14 +0600

    pagination added

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ dns.setServers(['8.8.8.8', '8.8.4.4']);
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -27,15 +27,61 @@ async function run() {
   try {
     await client.connect();
     
-    const database = client.db("BiblioDrop_DB");
+    const database = client.db("BiblioDrop-DB");
     const booksCollection = database.collection("books-collection");
 
+
+    // --- Admin Collection Definition
+    const usersCollection = database.collection("users-collection");
+    const transactionsCollection = database.collection("transactions-collection");
+
+    // --- Admin Routes
+    app.get('/admin/pending-books', async (req, res) => {
+      const result = await booksCollection.find({ status: "Pending Approval" }).toArray();
+      res.send(result);
+    });
+
+
+    app.patch('/admin/approve-book/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = { $set: { status: "Published" } };
+      const result = await booksCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
+    app.get('/admin/users', async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.patch('/admin/update-user-role/:id', async (req, res) => {
+      const id = req.params.id;
+      const { role } = req.body;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = { $set: { role: role } };
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
+
+    app.get('/admin/transactions', async (req, res) => {
+      const result = await transactionsCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get('/admin/stats', async (req, res) => {
+      const totalUsers = await usersCollection.countDocuments();
+      const totalBooks = await booksCollection.countDocuments();
+      res.send({ totalUsers, totalBooks });
+    });
+
+ 
     app.post('/books', async (req, res) => {
       const book = req.body;
       const result = await booksCollection.insertOne(book);
       res.send(result);
     });
-
 
     app.get('/books', async (req, res) => {
       const page = parseInt(req.query.page) || 1; 
@@ -51,6 +97,31 @@ async function run() {
         totalBooks,
         totalPages: Math.ceil(totalBooks / limit)
       });
+    });
+
+    app.get('/books/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await booksCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.delete('/books/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await booksCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    app.patch('/books/:id', async (req, res) => {
+      const id = req.params.id;
+      const updatedData = req.body;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: { status: updatedData.status },
+      };
+      const result = await booksCollection.updateOne(filter, updateDoc);
+      res.send(result);
     });
 
     console.log("Successfully connected to MongoDB!");
